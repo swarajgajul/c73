dd07663a90f87f1f31fabee4fb704d438c3168b0
swarajgajul <134145942+swarajgajul@users.noreply.github.com>
2024-03-17 11:55:20 +0530

    Add files via upload

---
+++ screens/Transaction.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default class TransactionScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: ""
+    };
+  }
+
+  getCameraPermissions = async domState => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+      hasCameraPermissions: status === "granted",
+      domState: domState,
+      scanned: false
+    });
+  };
+handlebarcodescan=async({type,data})=>{
+  this.setState({
+    scannedData:data,
+    domState:'normal',
+    scanned:true
+  })
+
+}
+  render() {
+    const{domState,hasCameraPermissions,scannedData,scanned}=this.state
+if (domState==="scanner"){
+  return(
+    <BarCodeScanner style={StyleSheet.absoluteFillObject}
+    onBarCodeScanned={scanned?undefined:this.handlebarcodescan}>
+    </BarCodeScanner>
+  )
+}
+    return (
+      <View style={styles.container}>
+        
+        <TouchableOpacity
+          style={[styles.button, { marginTop: 25 }]}
+          onPress={() => this.getCameraPermissions("scanner")}
+        >
+          <Text style={styles.buttonText}>Scan QR Code</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5653D4"
+  },
+  text: {
+    color: "#ffff",
+    fontSize: 15
+  },
+ 
+});
